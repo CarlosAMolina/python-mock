b44dcb51701509f43cf538cc84131f4d15eef16c
carlosamolina <15368012+CarlosAMolina@users.noreply.github.com>
2020-12-28 20:02:05 +0100

    Refactor change method declaration

--- mock_class_method/classes.py
+++ mock_class_method/classes.py
@@ -1,9 +1,9 @@
 class Calculator:
 
     def sum_3_to_number(self, number):
-        return number + self._get_number_three()
+        return number + self._get_number_3()
 
     @staticmethod
-    def _get_number_three():
+    def _get_number_3():
         return 3
 
--- mock_class_method/test.py
+++ mock_class_method/test.py
@@ -10,9 +10,9 @@ class TestCalculator(unittest.TestCase):
         calculator = classes.Calculator()
         self.assertEqual(calculator.sum_3_to_number(3), 6, "Should be 6")
 
-    @mock.patch('classes.Calculator._get_number_three')
-    def test_mocked_sum_3_to_number(self, _get_number_three):
-        _get_number_three.return_value = 4
+    @mock.patch('classes.Calculator._get_number_3')
+    def test_mocked_sum_3_to_number(self, _get_number_3):
+        _get_number_3.return_value = 4
         calculator = classes.Calculator()
         self.assertEqual(calculator.sum_3_to_number(3), 7, "Should be 7")
 
